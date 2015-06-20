97361094c9c64111e7da5dce0e50bb3050e30e64
Uche <chiedu@gmx.co.uk>
2015-06-20 15:02:33 +0100

    3_commit

--- Assignment2c.R
+++ Assignment2c.R
@@ -12,6 +12,7 @@ FluTrend1 = lm(log(ILI)~Queries, data=FluTrain)
 summary(FluTrend1)
 #summary(FluTrend2)
 cor(logil ,FluTrain$Queries)
+cor(logil ,FluTrain$Queries)^2
 FluTest = read.csv("FluTest.csv", header = T)
 summary(FluTest)
 PredTest1 = exp(predict(FluTrend1, newdata=FluTest))
@@ -22,15 +23,21 @@ PredTest1[11]
 SSE = sum((PredTest1 -FluTest$ILI)^2)
 RMSE = sqrt(SSE/nrow(FluTest))
 RMSE
-install.packages("zoo")
+# install.packages("zoo")
 library(zoo)
 ILILag2 = lag(zoo(FluTrain$ILI), -2, na.pad=TRUE)
 FluTrain$ILILag2 = coredata(ILILag2)
 table(is.na(FluTrain$ILILag2))
 plot( log (FluTrain$ILILag2), log(FluTrain$ILI))
-
 FluTrend2 = lm(log(ILI)~Queries + log(ILILag2), data=FluTrain)
 summary(FluTrend2)
 ILILag2 = lag(zoo(FluTest$ILI), -2, na.pad=TRUE)
 FluTest$ILILag2 = coredata(ILILag2)
 table(is.na(FluTest$ILILag2))
+FluTest$ILILag2[1] = FluTrain$ILI[416]
+FluTest$ILILag2[2] = FluTrain$ILI[417]
+PredTest2 = exp(predict(FluTrend2, newdata=FluTest))
+summary(PredTest2)
+SSE2 = sum((PredTest2 -FluTest$ILI)^2)
+RMSE2 = sqrt(SSE2/nrow(FluTest))
+RMSE2
